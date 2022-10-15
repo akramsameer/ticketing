162dfbd8a2d9c9d1382b2d29ec71c0bc1db02617
Akram Samir <akram.sameer99@gmail.com>
2022-10-15 03:58:10 +0200

    adding common lib

---
+++ common/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const color = {
+    red: 10,
+};
+exports.default = color;
---
+++ common/src/index.ts
@@ -0,0 +1,9 @@
+interface Color {
+  red: number;
+}
+
+const color: Color = {
+  red: 10,
+};
+
+export default color;
